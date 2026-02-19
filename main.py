6159e18ff0f450542cf3dc0eb3e225b3024308ee
Tim Bedin <captainceramic@gmail.com>
2026-02-19 11:26:05 +1100

    Altered to use command-line arguments

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
+
+import sys
+import pathlib
+
 from pdf2image import convert_from_path
 import pytesseract
 
-pages = convert_from_path("./docs/Mulesing article.pdf", dpi=600)
+# I need to get the name of the document from the
+# command line arguments.
 
-output_file = "output.txt"
+input_doc_name = sys.argv[1]
 
+pages = convert_from_path(input_doc_name, dpi=600)
+output_file = f"./output/{input_doc_name.replace('.pdf', '.txt')}"
 
+pathlib.Path("./output").mkdir(exist_ok=True)
 with open(output_file, "w", encoding="utf-8") as f:
     for i, page in enumerate(pages):
         text = pytesseract.image_to_string(page)
